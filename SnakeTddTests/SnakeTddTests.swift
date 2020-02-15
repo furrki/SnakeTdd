31f0dcb1668f64c5bd6d36dadaf9f37fdb6c99f3
furrki <furkankaynary@gmail.com>
2020-02-15 22:38:15 +0200

    Init snake tests

--- SnakeTddTests/SnakeTddTests.swift
+++ SnakeTddTests/SnakeTddTests.swift
@@ -9,12 +9,22 @@
 import XCTest
 @testable import SnakeTdd
 
-class SnakeGameTddTests: XCTestCase {
+class SnakeInitializationTests: XCTestCase {
     func testInitGame() {
         let game: Game = Game(speed: 2, areaSize: CGSize(width: 100, height: 100))
         
         XCTAssertEqual(game.areaSize.width, 100)
         XCTAssertEqual(game.areaSize.height, 100)
         XCTAssertEqual(game.speed, 2)
+    }
+    
+    func testInitSnake() {
+        let snake: Snake = Snake(headPos: CGPoint(x: 10, y: 10), direction: .down, initialSize: 4)
+        
+        XCTAssertEqual(snake.headPos.x, 10)
+        XCTAssertEqual(snake.headPos.y, 10)
+        XCTAssertEqual(snake.direction, .down)
+        XCTAssertEqual(snake.tail.count, 3)
+        XCTAssertEqual(snake.tail, [CGPoint(x: 10, y: 9), CGPoint(x: 10, y: 8), CGPoint(x: 10, y: 7)])
     }
 }

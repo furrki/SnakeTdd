d2e705e069da36f539451752199e50b81af282c9
furrki <furkankaynary@gmail.com>
2020-02-15 22:25:47 +0200

    Green test for initializing game

---
+++ SnakeTdd/Game.swift
@@ -0,0 +1,19 @@
+//
+//  Game.swift
+//  SnakeTdd
+//
+//  Created by Furkan Kaynar on 15.02.2020.
+//  Copyright © 2020 Furkan Kaynar. All rights reserved.
+//
+
+import UIKit
+
+class Game {
+    private(set) var speed: Int
+    private(set) var areaSize: CGSize
+    
+    init(speed: Int, areaSize: CGSize) {
+        self.speed = speed
+        self.areaSize = areaSize
+    }
+}

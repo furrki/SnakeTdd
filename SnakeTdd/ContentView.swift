63e1e4cd2a9664d6b6caf00f3ccd2e04e94ee45a
furrki <furkankaynary@gmail.com>
2020-02-15 21:55:02 +0200

    Initial Commit

---
+++ SnakeTdd/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnakeTdd
+//
+//  Created by Furkan Kaynar on 15.02.2020.
+//  Copyright © 2020 Furkan Kaynar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

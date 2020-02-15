061a3afe4aaba37b4b8c81203b67ad8d1dd56b54
furrki <furkankaynary@gmail.com>
2020-02-15 22:25:09 +0200

    Red Tests for game init

--- SnakeTddTests/SnakeTddTests.swift
+++ SnakeTddTests/SnakeTddTests.swift
@@ -9,26 +9,12 @@
 import XCTest
 @testable import SnakeTdd
 
-class SnakeTddTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class SnakeGameTddTests: XCTestCase {
+    func testInitGame() {
+        let game: Game = Game(speed: 2, areaSize: CGSize(width: 100, height: 100))
+        
+        XCTAssertEqual(game.areaSize.width, 100)
+        XCTAssertEqual(game.areaSize.height, 100)
+        XCTAssertEqual(game.speed, 2)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }

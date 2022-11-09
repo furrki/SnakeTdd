9c0c5c470cfa3e689c0c290941b319a57e60a862
Furkan Kaynar <furkankaynary@gmail.com>
2022-11-10 00:02:00 +0200

    clear git cache

--- Pods/Target Support Files/Pods-SnakeTdd-SnakeTddUITests/Pods-SnakeTdd-SnakeTddUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SnakeTdd_SnakeTddUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SnakeTdd_SnakeTddUITests
-@end
--- Pods/Target Support Files/Pods-SnakeTdd/Pods-SnakeTdd-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SnakeTdd : NSObject
-@end
-@implementation PodsDummy_Pods_SnakeTdd
-@end
